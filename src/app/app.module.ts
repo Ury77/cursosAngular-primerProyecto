c97de38eaa48934f5bb87cfed7412b97ed1828e7
Uriel Lopez <uriel.lopez7719@gmail.com>
2020-09-25 02:19:08 -0500

    Creando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CardComponent } from './components/card/card.component';
+import { InfoComponent } from './components/info/info.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    CardComponent,
+    InfoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
